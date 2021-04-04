1d5a447205ee91e8dfb047f589b81b62a9b97e01
borricha <naross@naver.com>
2021-04-04 21:40:19 +0900

    과제

--- newproject/posts/views.py
+++ newproject/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+       # response.status_code = 200
+        return HttpResponse("{\"req_method\":\"get\"}")
+    elif request.method == "POST":
+       # response.status_code = 200
+        return HttpResponse("{\"req_method\":\'post\"}")
+    else:
+        HttpResponse.status_code = 405
+        return HttpResponse("{\"error\":\"request method not allowed\"}")
